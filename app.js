8beb1d62df4f95f21c5879480ddcae194523ad8f
Saksham Jain <sakshamjain114@gmail.com>
2022-07-19 21:28:28 +0530

    (wip): deployment

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
 
-
-
 const app = express();
 
+dotenv.config({ path: "./config.env" });
 
-dotenv.config({ path: './config.env' })
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 3000;
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 
-
-const User = require('./models/user')
-require('./DATABSE/db.js');
+const User = require("./models/user");
+require("./DATABSE/db.js");
 app.use(express.json());
-app.use(require('./Router/auth'));
+app.use("/api", require("./Router/auth"));
 
 // const middleware = (req,res,next)=>{
 //     console.log('miidleware')
@@ -26,25 +32,22 @@ app.use(require('./Router/auth'));
 //   res.send('Hello Wokrld! app')
 // })
 
-
-
 // app.get('/aboutme', (req, res) => {
 //     res.send('Hello Wokrld! profile')
 //   })
-  
+
 // app.get('/login', (req, res) => {
 //     res.send('Hello Wokrld! login')
 //   })
- 
+
 // app.get('/signup', (req, res) => {
 //     res.send('Hello Wokrld! signup')
 //   })
 
-
-if( process.env.NODE_ENV=="production"){
-  app.use(express.static("fe/build"));
-}
+// if( process.env.NODE_ENV=="production"){
+//   app.use(express.static("fe/build"));
+// }
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,125 @@
+import { Typography } from "@mui/material";
+import React, { useState, useRef, useEffect } from "react";
+import emailjs from "@emailjs/browser";
+
+const Chat = () => {
+  const [userdata, setUserdata] = useState(false);
+
+  const callchatpage = async () => {
+    try {
+      const res = await fetch("/api//chat", {
+        method: "GET",
+        header: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      setUserdata(data);
+      if (!res.status === 401) {
+        window.alert("error here");
+      }
+    } catch (error) {
+      window.alert("Login For Best Experience");
+    }
+  };
+
+  useEffect(() => {
+    callchatpage();
+  });
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_z83wjzk",
+        "template_xmk7zm6",
+        form.current,
+        "IdxkysMK1LwB52jll"
+      )
+      .then(
+        (result) => {
+          window.alert("Sent");
+          console.log(result);
+        },
+        (error) => {
+          window.alert("failed");
+          console.log(error);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <form method="GET" ref={form} onSubmit={sendEmail}>
+      <Typography>
+        <div className="contactusDiv">
+          <div className="contactbox">
+            <div className="formdiv">
+              <div className="getintouch">
+                <h2>Get In Touch</h2>
+              </div>
+              <div className="contactus">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="inputContact"
+                  placeholder="Name"
+                  value={userdata.name}
+                  required
+                />
+              </div>
+              <div className="contactus">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="inputContact"
+                  placeholder="Email"
+                  value={userdata.email}
+                  required
+                />
+              </div>
+              <div className="contactus">
+                <input
+                  type="phone"
+                  id="phone"
+                  name="phone"
+                  className="inputContact"
+                  placeholder="Phone"
+                  value={userdata.phone}
+                  required
+                />
+              </div>
+              <div className="contactus">
+                <textarea
+                  id="message"
+                  name="message"
+                  className="inputContact"
+                  placeholder="Message"
+                  value={userdata.message}
+                  required
+                />
+              </div>
+              <div className="contactbutton">
+                <button type="submit">
+                  <Typography>Submit</Typography>
+                </button>
+              </div>
+            </div>
+
+            <img
+              alt="img"
+              className="contactimg"
+              src="https://www.travelyourwaydirect.com/oshutoon/2022/03/contact-us.png"
+            />
+          </div>
+        </div>
+      </Typography>
+    </form>
+  );
+};
+
+export default Chat;
